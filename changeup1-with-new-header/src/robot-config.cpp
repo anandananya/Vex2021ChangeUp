401e8aac511eaae5580d4f478da340b6597e1ab6
ashna-khemani <44122295+ashna-khemani@users.noreply.github.com>
2020-11-13 19:59:07 -0800

    Created new original header file
    I added a new header file "bot-functions.h" in the "include" folder that has declarations of our drive functions. The functions are now defined in "bot-functions.cpp" in the "src" folder instead of the "main.cpp".

---
+++ changeup1-with-new-header/src/robot-config.cpp
@@ -0,0 +1,32 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor BackLeftMotor = motor(PORT20, ratio18_1, false);
+motor BackRightMotor = motor(PORT2, ratio18_1, true);
+motor FrontLeftMotor = motor(PORT5, ratio18_1, false);
+motor FrontRightMotor = motor(PORT16, ratio18_1, true);
+motor LeftIntake = motor(PORT1, ratio18_1, false);
+motor RightIntake = motor(PORT3, ratio18_1, true);
+motor Roller = motor(PORT8, ratio18_1, true);
+motor Shooter = motor(PORT11, ratio18_1, true);
+controller mainControl = controller(primary);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
